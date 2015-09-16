0b5e95657102b0c0a93848cdd289f2ace9361f5c
Andrei Volchenko <volchenko.andrei@gmail.com>
2015-09-16 23:13:54 +0300

    huge school repo update

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	index: {
+		page: {
+			title: 'Rolling Scopes School'
+		},
+		talks: require('../data/talks.json')
+	},
+	'js-webinar': {
+		page: {
+			title: 'JS Webinar'
+		},
+		talks: require('../data/js-webinar/talks.json')
+	},
+	archive: {
+		page: {
+			title: 'Archive'
+		},
+		talks: require('../data/archive/2015-Q2/talks.json')
+	}
+}
